64a677a6917287c031f9e32c3a4e9078b6378bf1
ADPriceless <47830638+ADPriceless@users.noreply.github.com>
2023-09-11 19:24:31 +0100

    Experiment whether distance to coast helps

--- data_augmentation/coords.py
+++ data_augmentation/coords.py
@@ -1,10 +1,12 @@
+import pickle
+
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cluster import KMeans
 
 
 class NearestCluster(BaseEstimator, TransformerMixin):
-    def __init__(self, n_clusters) -> None:
+    def __init__(self, n_clusters: int) -> None:
         super().__init__()
         self.kmeans = KMeans(
             n_clusters=n_clusters,
@@ -21,4 +23,23 @@ class NearestCluster(BaseEstimator, TransformerMixin):
         centers = np.array([self.kmeans.cluster_centers_[pred] for pred in predictions])
         X['NearestClustLat'] = centers[:, 0]
         X['NearestClustLon'] = centers[:, 1]
+        return X
+
+
+class DistToCoastLoader(BaseEstimator, TransformerMixin):
+    def __init__(self, filepath: str, extra_data: bool) -> None:
+        super().__init__()
+        self.filepath = filepath
+        self.extra_data = extra_data
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        with open(self.filepath, 'rb') as pckl:
+            dist_to_coast = pickle.load(pckl)
+        if self.extra_data:
+            X['DistToCoast'] = dist_to_coast
+        else:
+            X['DistToCoast'] = dist_to_coast[:X.shape[0]]
         return X
