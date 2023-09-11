a1a7b38386a7173172786e26ad2094dc3bad9665
ADPriceless <47830638+ADPriceless@users.noreply.github.com>
2023-09-11 20:03:42 +0100

    Experiment with distance to city feature

--- data_augmentation/coords.py
+++ data_augmentation/coords.py
@@ -43,3 +43,28 @@ class DistToCoastLoader(BaseEstimator, TransformerMixin):
         else:
             X['DistToCoast'] = dist_to_coast[:X.shape[0]]
         return X
+
+
+class DistToCity(BaseEstimator, TransformerMixin):
+    def __init__(self, city_coords: list[np.array]) -> None:
+        super().__init__()
+        self.city_coords = city_coords
+        self.dist_to_city = None
+
+    def fit(self, X, y=None):
+        distances = np.zeros([X.shape[0], len(self.city_coords)])
+        for i, city_coord in enumerate(self.city_coords):
+            house_coord = X.loc[:, ['Longitude', 'Latitude']].to_numpy()
+            manhattan = house_coord - np.expand_dims(city_coord, axis=0)
+            distances[:, i] = np.linalg.norm(manhattan, ord=2, axis=1, keepdims=True).reshape(-1)
+        for i in range(len(self.city_coords) - 1):
+            self.dist_to_city = np.where(
+                distances[:, i] < distances[:, i + 1],
+                distances[:, i],
+                distances[:, i + 1]
+            )
+        return self
+
+    def transform(self, X):
+        X['DistToMajorCity'] = self.dist_to_city
+        return X
